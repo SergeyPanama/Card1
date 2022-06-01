74651296377c772876d761030f6ef7b40d3a52e6
Сергей Казаков <kazakov_serg@mail.ru>
2022-06-01 16:59:45 +0300

    initial commit

--- src/test/java/ru/netology/rest/HappyPath.java
+++ src/test/java/ru/netology/rest/HappyPath.java
@@ -1,33 +1,46 @@
 package ru.netology.rest;
 
 import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class HappyPath {
 
-private WebDriver driver;
+private static WebDriver driver;
+
     @BeforeAll
-    static void setUp(){
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+    static void setUpAll() {
+        //System.setProperty("driver.chrome.driver", "driver//chromedriver");
+        WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeEach
-    public void openPage() {
-        driver = new ChromeDriver();
-
-    }
     @AfterEach
     public void close(){
         driver.quit();
         driver = null;
+    }
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
     }
 
     @Test
@@ -43,6 +56,7 @@ private WebDriver driver;
 
     @Test
     public void shouldReturnValidValueOneName() {
+        open("http://localhost:7777/");
         $(".form-field_theme_alfa-on-white");
         $("[data-test-id=name] input").setValue("Алексей");
         $("[data-test-id=phone] input").setValue("+79012345678");
@@ -53,6 +67,7 @@ private WebDriver driver;
 
     @Test
     public void shouldReturnValidValueDoubleName() {
+        open("http://localhost:7777/");
         $(".form-field_theme_alfa-on-white");
         $("[data-test-id=name] input").setValue("Алексей Александр Иванов");
         $("[data-test-id=phone] input").setValue("+79012345678");
