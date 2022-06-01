d5299d1633763f205d94e5b320ddfaac9cbb432e
SergeyPanama <97026373+SergeyPanama@users.noreply.github.com>
2022-06-01 19:42:55 +0300

    Update HappyPath.java

--- src/test/java/ru/netology/rest/HappyPath.java
+++ src/test/java/ru/netology/rest/HappyPath.java
@@ -17,7 +17,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class HappyPath {
 
+private static WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll() {
+        //System.setProperty("driver.chrome.driver", "driver//chromedriver");
+        WebDriverManager.chromedriver().setup();
+    }
+    
     @AfterEach
     public void close(){
         driver.quit();
